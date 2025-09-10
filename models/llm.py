65e97e65980e5a71310b7f0b9ae761ff60e5b4d6
ABHINAV PENCHALA <161202161+AbhinavPenchala@users.noreply.github.com>
2025-09-11 00:17:34 +0530

    Update llm.py

--- models/llm.py
+++ models/llm.py
@@ -1,11 +1,1 @@
-'''from google.generativeai import Client
-from config.config import GOOGLE_GEMINI_API_KEY
 
-def get_gemini_model():
-    """Initialize and return the Google Gemini client."""
-    try:
-        client = Client(api_key=GOOGLE_GEMINI_API_KEY)
-        return client
-    except Exception as e:
-        print(f"Error initializing Gemini client: {e}")
-        return None'''
