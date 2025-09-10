a457d8abbeb97e5cc5bd2239703c803004e1df77
ABHINAV PENCHALA <161202161+AbhinavPenchala@users.noreply.github.com>
2025-09-11 00:01:13 +0530

    Update llm.py

--- models/llm.py
+++ models/llm.py
@@ -1,12 +1,6 @@
-
 from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
 from config.config import GOOGLE_GEMINI_API_KEY
 
 def get_gemini_model():
-    """Initialize and return the Google Gemini chat model."""
-    try:
-        model = ChatGoogleGenerativeAI(api_key=GOOGLE_GEMINI_API_KEY, model="gemini-1.5-pro")
-        return model
-    except Exception as e:
-        print(f"Error initializing Gemini model: {e}")
-        return None
+    model = ChatGoogleGenerativeAI(api_key=GOOGLE_GEMINI_API_KEY, model="gemini-1.5-pro")
+    return model
